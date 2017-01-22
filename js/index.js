1efd9a4cf96fff8a745a8ba562eeccdea69a6322
都基鹏 <dylandu123@163.com>
2017-01-22 16:32:48 +0800

    #

--- js/index.js
+++ js/index.js
@@ -9,30 +9,41 @@ var Main = {
     blockFactory: null,
     blocks: [],
     time: 0,
+    timeQueue: null,
     init: function() { //初始化
         Tool.file.imgs(["img/man.png", "img/block.png", "img/move.png", "img/thorn.png", "img/flip.png", "img/thorn_bg.png"], function(imgs) {
-            Tool.getId("js_start_loading").style.display = "none";
-            Tool.getId("js_start_btn").style.display = "block";
+            Tool.getID("js_start_loading").style.display = "none";
+            Tool.getID("js_start_btn").style.display = "block";
             Main.imgs = imgs;
-            var canvas = Tool.getId("canvas");
-            Main.gameInfo.w = canvas.offsetWidth; // 偏移宽度
-            Main.gameInfo.h = canvas.offsetHeight; // 偏移高度
-            Main.cxt = canvas.getContext("2d"); //可以在页面绘制图形的对象
+            var canvas = Tool.getID("canvas");
+            Main.gameInfo.w = canvas.offsetWidth;
+            Main.gameInfo.h = canvas.offsetHeight;
+            Main.cxt = canvas.getContext("2d");
+            Main.person = new Person(150, 0, Main.imgs[0], Main.cxt, Main.gameInfo);
+            Main.blockFactory = new BlockFactory({
+                block: Main.imgs[1],
+                move: Main.imgs[2],
+                flip: Main.imgs[4],
+                thorn: Main.imgs[5],
+                cxt: Main.cxt,
+                gameInfo: Main.gameInfo,
+            });
+            Main.initEvent();
         });
     },
     start: function() {
-        Tool.getId('js_end_flush').style.display = "none";
-        Tool.getId("js_start_flush").style.display = "none";
-        Main.initPerson();
-        Main.initBlock();
-        Main.initEvent();
-        Main.process();
+        Tool.getID("js_start_flush").style.display = "none";
+        var timeQueue = new Tool.time.TimeProcess();
+        timeQueue.add(null, null, Main.updata);
+        timeQueue.add(null, null, Main.draw);
+        timeQueue.st();
+        this.timeQueue = timeQueue;
     },
     initEvent: function() { //初始化事件
-        Tool.getId("js_main").onkeydown = function(e) {
+        Tool.getID("js_main").onkeydown = function(e) {
             Main.keyDown(e);
         };
-        Tool.getId("js_main").onkeyup = function(e) {
+        Tool.getID("js_main").onkeyup = function(e) {
             Main.keyUp(e);
         };
     },
@@ -51,76 +62,39 @@ var Main = {
         }
         e.preventDefault();
     },
-    replay: function() {
-        Main.cxt.clearRect(0, 0, Main.gameInfo.w, Main.gameInfo.h);
-        Main.start();
-    },
-    initPerson: function() {
-        Main.person = new Person(150, 0, Main.imgs[0], Main.cxt, Main.gameInfo);
-    },
-    initBlock: function() {
-        Main.blockFactory = new BlockFactory({
-        	block:Main.imgs[1],
-        	thorn:Main.imgs[3]
+    updata: function() {
+        Main.person.updata();
+        Main.time++;
+        if (Main.time > 40) {
+            Main.time = 0;
+            Main.blocks.push(Main.blockFactory.creat());
         }
-        , Main.cxt
-        , Main.gameInfo);
-        var block = Main.blockFactory.creater(120);
-        Main.blocks.push(block);
-    },
-    process: function() { //游戏运行
-        var tq = new Tool.time.TimeProcess();
-        tq.add(Main.draw, null, Main);
-        tq.add(Main.update, null, Main);
-        this.timeQuene = tq; //游戏时间进程
-        this.timeQuene.start();
+        for (var i = 0; i < Main.blocks.length; i++) {
+            var block = Main.blocks[i];
+            if (!block) continue;
+            if (!block.checkLimit()) {
+                Main.blocks.splice(Main.blocks.indexOf(block), 1);
+                block = null;
+                i--;
+                continue;
+            }
+            block.updata();
+        }
     },
     draw: function() {
-        Main.cxt.clearRect(0, 0, Main.gameInfo.w, Main.gameInfo.h); //清除图像
+        Main.cxt.clearRect(0, 0, Main.gameInfo.w, Main.gameInfo.h);
+        Main.drawThron();
         Main.person.draw();
-        Main.drawThornBg();
         for (var i = 0; i < Main.blocks.length; i++) {
-            block = Main.blocks[i];
+            var block = Main.blocks[i];
+            if (!block) continue;
             block.draw();
         }
     },
-    update: function() {
-        Main.time++;
-        if (Main.time >= 50) {
-            Main.time = 0;
-            var block = Main.blockFactory.creater();
-            Main.blocks.push(block);
-        }
-        for (var i = 0; i < Main.blocks.length; i++) {
-            block = Main.blocks[i];
-            block.update();
-            if (block.checkMap()) {
-            	Main.blocks.splice(Main.blocks.indexOf(block),1);
-            	i--;
-            	continue;
-            }
-            Main.person.checkBlockOn(block);
-        }
-        Tool.getId("js_life").style.width = Main.person.life + "px";
-        Tool.getId("js_level").innerHTML = Main.person.level;
-        Main.person.update();
-        if (Main.person.isDead) {
-            Main.over();
-        }
-    },
-    drawThornBg: function() {
+    drawThron: function() {
         for (var i = 0; i <= 35; i++) {
             Main.cxt.drawImage(Main.imgs[5], 0, 0, 18, 21, i * 9, 0, 9, 11);
         }
-    },
-    over: function() {
-        Tool.getId('js_end_flush').style.display = "block";
-        Tool.getId("js_end_flush").getElementsByTagName("p")[0].innerHTML = "你牛B呀,下了<label>" + Main.person.level + "</label>层,男人中的男人呀！";
-        Tool.getId("js_end_flush").getElementsByTagName("a")[0].innerHTML = "想更男人一点";
-        Tool.getId("js_end_flush").getElementsByTagName("span")[0].className = "icon happy";
-        Tool.getId("js_life").style.width = "0px";
-        Main.blocks = [];
-        this.timeQuene.stop();
     }
 };
 Main.init();
--- js/tool.js
+++ js/tool.js
@@ -1,242 +1,185 @@
 var Tool = function() {
-    function getId(id) {
-        return typeof id == "string" ? document.getElementById(id) : id;
-    }
-
-    function reg(space, obj) { //命名空间
-        var namespace = exports[space] || {};
+    function reg(name_space, obj) {
+        var namespace = exports[name_space] || {};
         for (var key in obj) {
             namespace[key] = obj[key];
         }
-        exports[space] = namespace;
+        exports[name_space] = namespace;
+    }
+
+    function getID(id) {
+        return typeof id == "string" ? document.getElementById(id) : id;
     }
     var exports = {
-        getId: getId,
-        reg: reg
-    };
+        getID: getID,
+        reg: reg,
+    }
     return exports;
 }();
-Tool.reg('file', function() {
-    function imgs(arrUrl, cb) {
+Tool.reg("file", function() {
+    function img(imgs, cb) {
         var count = 0;
-        var imgs = []; //返回图片集合
-        for (var i = 0; i < arrUrl.length; i++) {
-            var img = new Image();
-            img.onload = function() {
+        imgAry = [];
+        for (var i = 0; i < imgs.length; i++) {
+            var Img = new Image();
+            Img.src = imgs[i];
+            Img.index = i;
+            Img.onload = function() {
                 this.onload = null;
-                imgs.push(this);
-                count += 1;
-                img = null;
-                if (count >= arrUrl.length) {
-                    imgs.sort(function(a, b) {
+                imgAry.push(this);
+                count++;
+                if (count >= imgs.length) {
+                    imgAry.sort(function(a, b) {
                         return a.index - b.index;
-                    });; //自定义函数
-                    cb && cb(imgs);
+                    })
+                    cb && cb(imgAry);
                 }
             }
-            img.index = i;
-            img.src = arrUrl[i];
         }
     }
     var exports = {
-        imgs: imgs,
-    }
-    return exports;
-}());
-Tool.reg('sprite', function() {
-    var Sprite = function(img, cxt, fps, param) {
-        this.animations = {};
-        this.img = img;
-        this.cxt = cxt;
-        this.x = param.x || 0;
-        this.y = param.y || 0;
-        this.fps = fps; //每秒帧数
-        this.xspeed = param.xspeed || 0; //x轴加速度
-        this.yspeed = param.yspeed || 0; //y轴加速度
-        this.yaspeed = param.yaspeed || 0; //y轴 加速度 增量
-        this.lazy = 1000 / this.fps; //延迟
-        this.last = 0; //持续
-        this.moveLazy = 33; //延迟移动
-        this.moveLast = 0; //持续移动
-        //当前动画
-        this.index = null;
-        this.key = ""; //当前按键
-    }
-    Sprite.prototype = { //添加按键动画
-        add: function(key, animation) {
-            this.animations[key] = animation;
-            if (!this.index) {
-                this.index = animation;
-                this.key = key;
-            }
-        },
-        //绘画出当前帧
-        draw: function() {
-            if (!this.index || !this.img) return false;
-            var frame = this.index.current;
-            this.cxt.drawImage(this.img, frame.x, frame.y, frame.w, frame.h, this.x, this.y, frame.dw, frame.dh);
-        },
-        update: function() {
-            //当前时间
-            //返回指定的 Date 对象自 1970 年 1 月 1 日午夜（通用时间）以来的毫秒数
-            var t = new Date().getTime();
-            //时间差值 = 当前时间 - 持续时间(启动时间)
-            var diff = t - this.last;
-            //移动时间差值  = 当前时间 - 移动时间差值
-            var moveDiff = t - this.moveLast;
-            if (this.last == 0) { //持续时间(启动时间)
-                diff = this.lazy; //延迟时间
-                moveDiff = this.moveLazy; //移动延迟时间
-            }
-            if (diff >= this.lazy) { // 时间差值 >= 延迟时间
-                this.index.next(); //当前动画 下一帧
-                this.last = t; //持续时间(启动时间)
-            }
-            if (moveDiff >= this.moveLazy) { //移动时间差值 >= 移动延迟时间
-                //y轴 增量 非空	
-                if (this.yaspeed) this.yspeed += this.yaspeed; //y轴加速度 + y轴 增量
-                //x轴加速度 非空
-                if (this.xspeed) this.x += this.xspeed; //增加  x轴加速度 
-                //y轴加速度 非空
-                if (this.yspeed) this.y += this.yspeed; //增加 y轴加速度
-                this.moveLast = t; //移动延迟时间(启动时间)
-            }
-        },
-        change: function(dir) {
-            this.index = this.animations[dir];
-        },
-        move: function(x, y) {
-            this.x = x;
-            this.y = y;
-        },
-        size: function() {
-            var frame = this.index.current;
-            return {
-                w: frame.dw,
-                h: frame.dh,
-                x: this.x,
-                y: this.y,
-                r: this.x + frame.dw,
-                b: this.y + frame.dh
-            };
-        },
-        setYSpeed: function(yspeed, yaspeed) {
-            this.yspeed = yspeed;
-            this.yaspeed = yaspeed;
-        },
-        setXSpeed: function(xspeed) {
-            this.xspeed = xspeed;
-        }
-    }
-    var Frame = function(x, y, w, h, dw, dh) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.dw = dw;
-        this.dh = dh;
-    }
-    var Animation = function(param) {
-        this.startX = param.startX || 0;
-        this.startY = param.startY || 0;
-        this.fs = param.fs || 1;
-        this.sw = param.sw || 0;
-        this.sh = param.sh || 0;
-        this.width = param.width || param.sw;
-        this.height = param.height || param.sh;
-        this.dir = param.dir || "right";
-        this.loop = !!param.loop;
-        //this.fps = param.fps || 30;
-        //this.lazy = 1000 / this.fps;
-        //this.last = 0;
-        //存放帧图像的集合
-        this.ls = [];
-        //当前帧
-        this.current = null;
-        //当前帧得索引
-        this.index = -1;
-        this.init();
-    }
-    Animation.prototype = {
-        init: function() { //初始化帧动画(人物)
-            for (var i = 0; i < this.fs; i++) {
-                var x = this.startX + (this.dir == "right" ? i * this.sw : 0);
-                var y = this.startY + (this.dir == "down" ? i * this.sh : 0);
-                var frame = new Frame(x, y, this.sw, this.sh, this.width, this.height);
-                //将帧动画(人物)存入数组
-                this.ls.push(frame);
-            }
-            this.index = 0;
-            this.current = this.ls[0];
-        },
-        //下一帧
-        next: function() {
-            if (this.index + 1 >= this.ls.length) { //当前帧得索引+1 >= 存放的帧数
-                if (this.loop) { //是否循环
-                    //
-                    this.current = this.ls[0]; //当前帧
-                    this.index = 0; //当前帧得索引
-                }
-            } else { //当前帧得索引+1 < 存放的帧数
-                this.index += 1; //当前帧得索引++
-                this.current = this.ls[this.index]; //在存放的帧的集合【ls】中  获取  当前帧
-            }
-        },
-        //重置为第一帧
-        reset: function() {
-            this.current = this.ls[0]; //当前帧
-            this.index = 0; //当前帧得索引
-        },
-        size: function() { //人物大小
-            return {
-                w: this.width,
-                h: this.height
-            };
-        }
-    }
-    var exports = {
-        Sprite: Sprite,
-        Animation: Animation,
-        Frame: Frame,
+        imgs: img,
     }
     return exports;
 }());
 Tool.reg("time", function() {
-    //定义贞管理类，兼容
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function(cb) {
         setTimeout(cb, 1000 / 60)
     };
     var TimeProcess = function() {
         this.list = [];
-        this.isStart = false;
+        this.start = false;
     }
     TimeProcess.prototype = {
-        add: function(cb, param, context) {
+        add: function(param, context, cb) {
             this.list.push({
                 cb: cb,
                 param: param,
                 context: context
             });
         },
-        start: function() {
-            this.isStart = true;
-            var self = this; //TimeProcess
+        st: function() {
+            this.start = true;
+            var self = this;
             requestAnimationFrame(function() {
-                var item = null,
-                    p = [];
                 for (var i = 0; i < self.list.length; i++) {
-                    item = self.list[i]; //timeQuene 游戏时间进程中需要做的项目   
-                    item.cb.apply(item.context, item.param); //1、Main.draw  2、Main.update
+                    var item = self.list[i]; //timeQuene 游戏时间进程中需要做的项目   
+                    item.cb(item.context, item.param); //1、Main.draw  2、Main.update
                 }
-                if (self.isStart) requestAnimationFrame(arguments.callee);
-            });
+                if (self.start) requestAnimationFrame(arguments.callee);
+            })
         },
         stop: function() {
-            this.isStart = false;
+            this.start = false;
         }
-    }
+    };
     var exports = {
         TimeProcess: TimeProcess
+    };
+    return exports;
+}());
+Tool.reg("spirit", function() {
+    var Frame = function(argument) {
+        this.sx = argument.sx || 0;
+        this.sy = argument.sy || 0;
+        this.sw = argument.sw || 0;
+        this.sh = argument.sh || 0;
+    }
+    var Animation = function(argument) {
+        this.fs = argument.fs || 1;
+        this.dir = argument.dir || "right";
+        this.frames = [];
+        this.currentframe = null;
+        this.index = 0;
+        this.init(argument);
+    }
+    Animation.prototype = {
+        init: function(argument) {
+            var startx = argument.startx;
+            var starty = argument.starty;
+            for (var i = 0; i < this.fs; i++) {
+                if (this.dir == "left") startx += argument.sw * -1 * i;
+                if (this.dir == "right") startx += argument.sw * i;
+                if (this.dir == "down") starty += argument.sh * i;
+                if (this.dir == "up") starty += argument.sh * -1 * i;
+                var frame = new Frame({
+                    sx: startx,
+                    sy: starty,
+                    sw: argument.sw,
+                    sh: argument.sh,
+                });
+                if (!this.currentframe) {
+                    this.currentframe = frame
+                    this.index = i;
+                };
+                this.frames.push(frame);
+            }
+        },
+        next: function() {
+            this.index++;
+            if (this.index >= this.frames.length) {
+                this.index = 0;
+            }
+            this.currentframe = this.frames[this.index];
+        }
+    };
+    var Spirit = function(argument) {
+        this.x = argument.x || 0;
+        this.y = argument.y || 0;
+        this.cxt = argument.cxt;
+        this.img = argument.img;
+        this.width = argument.w;
+        this.height = argument.h;
+        this.yspeed = argument.yspeed || 0;
+        this.yspeedup = argument.yspeedup || 0;
+        this.fps = argument.fps || 10;
+        this.xspeed = argument.xspeed || 0;
+        this.xspeedup = argument.xspeedup || 0;
+        this.animations = {};
+        this.currentanimation = null;
+        this.laze = 1000.0 / this.fps;
+        this.last = 0;
+        this.init();
+    }
+    Spirit.prototype = {
+        init: function() {
+            this.last = new Date().getTime();
+        },
+        updata: function() {
+            this.x += this.xspeed;
+            this.xspeed += this.xspeedup;
+            this.y += this.yspeed;
+            this.yspeed += this.yspeedup;
+            var t = new Date().getTime();
+            var diff = t - this.last;
+            if (diff > this.laze) {
+                this.last = t;
+                this.currentanimation.next();
+            }
+        },
+        draw: function() {
+            var frame = this.currentanimation.currentframe;
+            this.cxt.drawImage(this.img, frame.sx, frame.sy, frame.sw, frame.sh, this.x, this.y, this.width, this.height);
+        },
+        add: function(dir, animation) {
+            this.animations[dir] = animation;
+            if (!this.currentanimation) this.currentanimation = animation;
+        },
+        change: function(key) {
+            this.currentanimation = this.animations[key];
+        },
+        size: function() {
+            return {
+                x: this.x,
+                y: this.y,
+                w: this.width,
+                h: this.height
+            };
+        }
+    };
+    var exports = {
+        Spirit: Spirit,
+        Animation: Animation,
     };
     return exports;
 }());
